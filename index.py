5d9077ea8de9dc5012d393d76c87b16a022a306b
Ayano Tobaru <ayano.tobaru@mixi.co.jp>
2018-05-21 16:50:15 +0900

    ブクマとdaily rankingを取ってくるようにした

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from lib import naropy
+import datetime
 
 
 app = Flask(__name__)
@@ -9,16 +10,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    today = datetime.date.today()
     narou = naropy.Naropy()
+    rank_data = narou.ranking_daily(year=today.year, month=today.month, day=today.day)
     if request.method == 'POST':
         user_id = request.form['user_id']
         bookmark_id = request.form['bookmark_id']
         print("post")
-
+        bookmarks = narou.bookmark_get_all(userid=user_id, category=bookmark_id)
+        print(bookmarks)
         return render_template('ranking.html',
-                               user_id=user_id, bookmark_id=bookmark_id)
+                               user_id=user_id, bookmark_id=bookmark_id, rank_data=rank_data)
     else:
-        return render_template('ranking.html')
+        return render_template('ranking.html', rank_data=rank_data)
 
 
 if __name__ == '__main__':
